d7143ac49ecb81fb7da64ca9b513393532819f6b
gitifyyousuff <jerryyousuf@gmail.com>
2020-05-15 21:20:14 +0530

    git hub search user

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const NotFound = () => {
+  return (
+    <div>
+      <h1>
+        Not Found
+        <p className='lead'>The Page does not exist...</p>
+      </h1>
+    </div>
+  );
+};
+export default NotFound;
